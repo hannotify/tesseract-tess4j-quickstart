814b3e8ae15d67c6068b122ff76ef0e683ed5fe9
Hanno Embregts <hanno.embregts@infosupport.com>
2019-03-18 13:15:08 +0100

    Add low- and hires image

--- src/main/java/tesseract/tess4j/quickstart/Library.java
+++ src/main/java/tesseract/tess4j/quickstart/Library.java
@@ -3,6 +3,7 @@
  */
 package tesseract.tess4j.quickstart;
 
+import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
@@ -10,21 +11,20 @@ import java.io.File;
 
 public class Library {
     public static void main(String[] args) throws TesseractException {
-        String textCleanCode = doOCR("eng", new File("src/main/resources/clean-code.png"));
-        System.out.println("Done. Result:");
-        System.out.println(textCleanCode);
-
-        String textNewTestament = doOCR("grc", new File("src/main/resources/new-testament.png"));
-        System.out.println("Done. Result:");
-        System.out.println(textNewTestament);
+//        doOCR("eng", new File("src/main/resources/clean-code.png"));
+//        doOCR("grc", new File("src/main/resources/new-testament.png"));
+        doOCR("eng", new File("src/main/resources/joke-lowres.png"));
+        doOCR("eng", new File("src/main/resources/joke-hires.png"));
     }
 
-    private static String doOCR(String language, File image) throws TesseractException {
-        Tesseract tesseract = new Tesseract();
+    private static void doOCR(String language, File image) throws TesseractException {
+        ITesseract tesseract = new Tesseract();
         tesseract.setDatapath("/usr/local/Cellar/tesseract/4.0.0/share/tessdata/");
         tesseract.setLanguage(language);
 
         System.out.println(String.format("Processing image '%s', please wait...", image.getName()));
-        return tesseract.doOCR(image);
+        String text = tesseract.doOCR(image);
+        System.out.println("Done. Result:");
+        System.out.println(text);
     }
 }
